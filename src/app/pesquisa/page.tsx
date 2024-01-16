2308a808c54dc5006141de40f6e459578457241c
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2024-01-15 22:14:44 -0300

    feat: integrated filter with cms

--- src/app/pesquisa/page.tsx
+++ src/app/pesquisa/page.tsx
@@ -5,6 +5,7 @@ import { fetchHygraph } from "@/src/utils/fetchHygraph";
 import { ProductsSection } from "./components/ProductsSection";
 import { getProductsQueries } from "@/src/utils/getProductsQueries";
 import { transformLoanwords } from "@/src/utils/transformLoanwords";
+import { TProductType } from "@/src/types/ProductType";
 
 interface SearchProps {
   searchParams: { [key: string]: string };
@@ -13,12 +14,13 @@ interface SearchProps {
 export default async function Search({
   searchParams: { categoria, subcategoria, tipoDoProduto, q, pagina = "1" },
 }: SearchProps) {
-  const res = await fetchHygraph<{
+  const products = await fetchHygraph<{
     produtos: TProduct[];
     informacao: {
       produtosPorPagina: string;
     };
     categoriasParaFiltrar: TCategory[];
+    tipoDeProdutos: TProductType[]
   }>(`query ProductsQuery {
           produtos(${getProductsQueries({
             category: categoria,
@@ -34,7 +36,16 @@ export default async function Search({
               }
             }
           }
+        }`);
 
+  const infos = await fetchHygraph<{
+    produtos: TProduct[];
+    informacao: {
+      produtosPorPagina: string;
+    };
+    categoriasParaFiltrar: TCategory[];
+    tipoDeProdutos: TProductType[]
+  }>(`query InfosQuery {
           informacao(where: {id: "clpkiwoog0nk30blwftd2eo8i"}) {
             produtosPorPagina
           }
@@ -45,6 +56,14 @@ export default async function Search({
             subcategorias {
               nome
               id
+            }
+          }
+
+          tipoDeProdutos {
+            tipoDoProduto
+            id
+            cor {
+              hex
             }
           }
         }`);
@@ -60,11 +79,11 @@ export default async function Search({
         </header>
       )}
       <div className="w-full m-auto max-w-7xl flex flex-wrap gap-4 md:flex-nowrap">
-        <Categories categories={res.categoriasParaFiltrar} />
+        <Categories productTypes={infos?.tipoDeProdutos} categories={infos?.categoriasParaFiltrar} />
         <ProductsSection
           page={Number(pagina)}
-          products={res?.produtos ?? []}
-          productsPerPage={Number(res?.informacao?.produtosPorPagina)}
+          products={products?.produtos ?? []}
+          productsPerPage={Number(infos?.informacao?.produtosPorPagina)}
         />
       </div>
     </main>
